fa1de9d20b6573ae0c8fb61810c12a09cbab5b4c
Dovoyan <yankavalenkau@gmail.com>
2020-10-06 00:35:17 +0300

    feat: did test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let  b = String(n).split("");
+    let otv = '';
+    for (let i =0;i<b.length;i++){
+    if (typeof +b[i] == "number") otv = otv + b[i];
+
+}
+
+    otv = otv.split('').reverse().join('');
+    return parseInt(otv, 10) 
 }
